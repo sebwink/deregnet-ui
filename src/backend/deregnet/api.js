6a3d292fe264b0e2fa5ddcab5e597e083b2e5a3c
sebwink <winkler@informatik.uni-tuebingen.de>
2019-03-16 11:30:47 +0100

    cookie jwt

--- src/backend/deregnet/api.js
+++ src/backend/deregnet/api.js
@@ -1,4 +1,4 @@
-const API_ROOT = process.env.DEREGNET_API_ROOT || 'https://dereg.net/deregnet';
+const API_ROOT = process.env.DEREGNET_API_ROOT || 'https://dereg.net/api/ui';
 
 export default {
   root: API_ROOT,
